b99d9f8810780041e12ce8a376acaf929f265ced
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-07-27 23:36:49 +0530

    Library Commit

---
+++ app/src/main/java/com/example/sqlitesmallproject/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.sqlitesmallproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        add_button =findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
